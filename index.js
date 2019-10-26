7be093300e9372888e77c89b91cab59c6e23bda9
phips28 <phips28@gmx.at>
2019-10-26 21:48:02 +0200

    fix code formatting

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const { execSync } = require('child_process')
 
 // Run your GitHub Action!
 Toolkit.run(async tools => {
-
   const pkg = tools.getPackageJSON()
   const event = tools.context.payload
   // console.log('event:', event)
