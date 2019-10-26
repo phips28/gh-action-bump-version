814f393bb95dfded8fc65439f61b8f6f08790453
phips28 <phips28@gmx.at>
2019-10-26 20:01:14 +0200

    fixed script

--- gh-action-bump-version-run.js
+++ gh-action-bump-version-run.js
@@ -3,19 +3,25 @@ const fs = require('fs')
 const path = require('path')
 const { execSync } = require('child_process')
 
-// const event = JSON.parse(fs.readFileSync('/github/workflow/event.json').toString())
+const event = JSON.parse(fs.readFileSync('/github/workflow/event.json').toString())
 
 let pkg = require(path.join(process.cwd(), 'package.json'))
 
 const run = async () => {
-  // let messages = event.commits.map(commit => commit.message + '\n' + commit.body)
-  //
+  let messages = event.commits.map(commit => commit.message + '\n' + commit.body)
+
+  const commitMessage = 'version bump'
+  const isVersionBump = messages.map(message => message.toLowerCase().includes(commitMessage)).includes(true)
+  if (isVersionBump) {
+    return
+  }
+
   let version = 'patch'
-  // if (messages.map(message => message.includes('BREAKING CHANGE')).includes(true)) {
-  //   version = 'major'
-  // } else if (messages.map(message => message.toLowerCase().startsWith('feat')).includes(true)) {
-  //   version = 'minor'
-  // }
+  if (messages.map(message => message.includes('BREAKING CHANGE')).includes(true)) {
+    version = 'major'
+  } else if (messages.map(message => message.toLowerCase().startsWith('feat')).includes(true)) {
+    version = 'minor'
+  }
 
   const exec = str => {
     return process.stdout.write(execSync(str))
@@ -26,7 +32,7 @@ const run = async () => {
   console.log('current:', current, '/', 'version:', version)
   let newVersion = execSync(`npm version --git-tag-version=false ${version}`).toString()
   console.log('new version:', newVersion)
-  exec(`git commit -a -m 'ci: version bump ${newVersion}'`)
+  exec(`git commit -a -m 'ci: ${commitMessage} ${newVersion}'`)
   exec(`git push`)
   exec(`git tag ${newVersion}`)
   exec(`git push --tags`)
