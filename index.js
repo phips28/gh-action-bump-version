bcd1e5807166a3c6e6f9b62ce6f10699fcc6c29a
Cdimock <colbydimock@gmail.com>
2022-03-04 15:55:33 -0600

    updated: to read build number from package json properly

--- index.js
+++ index.js
@@ -133,7 +133,7 @@ const workspace = process.env.GITHUB_WORKSPACE;
   // GIT logic
   try {
     const currentVersion = pkg.version.toString();
-    const currentBuild = pkg.version.parseInt();
+    const currentBuild = pkg.buildNumber.parseInt();
     const newBuild = currentBuild + 1;
     // set git user
     await runInWorkspace('git', ['config', 'user.name', `"${process.env.GITHUB_USER || 'Automated Version Bump'}"`]);
