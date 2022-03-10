c1b590021d431ea132be7b6e7262c9fbf64bf458
Cdimock <colbydimock@gmail.com>
2022-03-09 23:02:39 -0500

    first removal

--- index.js
+++ index.js
@@ -47,13 +47,13 @@ const workspace = process.env.GITHUB_WORKSPACE;
   }
 
   // input wordings for MAJOR, MINOR, PATCH, PRE-RELEASE
-  const majorWords = process.env['INPUT_MAJOR-WORDING'].split(',');
-  const minorWords = process.env['INPUT_MINOR-WORDING'].split(',');
-  // patch is by default empty, and '' would always be true in the includes(''), thats why we handle it separately
-  const patchWords = process.env['INPUT_PATCH-WORDING'] ? process.env['INPUT_PATCH-WORDING'].split(',') : null;
-  const preReleaseWords = process.env['INPUT_RC-WORDING'] ? process.env['INPUT_RC-WORDING'].split(',') : null;
+  // const majorWords = process.env['INPUT_MAJOR-WORDING'].split(',');
+  // const minorWords = process.env['INPUT_MINOR-WORDING'].split(',');
+  // // patch is by default empty, and '' would always be true in the includes(''), thats why we handle it separately
+  // const patchWords = process.env['INPUT_PATCH-WORDING'] ? process.env['INPUT_PATCH-WORDING'].split(',') : null;
+  // const preReleaseWords = process.env['INPUT_RC-WORDING'] ? process.env['INPUT_RC-WORDING'].split(',') : null;
 
-  console.log('config words:', { majorWords, minorWords, patchWords, preReleaseWords });
+  // console.log('config words:', { majorWords, minorWords, patchWords, preReleaseWords });
 
   // get default version bump
   let version = process.env.INPUT_DEFAULT;
@@ -61,67 +61,35 @@ const workspace = process.env.GITHUB_WORKSPACE;
   // get the pre-release prefix specified in action
   let preid = process.env.INPUT_PREID;
 
-  // case: if wording for MAJOR found
-  if (
-    messages.some(
-      (message) => /^([a-zA-Z]+)(\(.+\))?(\!)\:/.test(message) || majorWords.some((word) => message.includes(word)),
-    )
-  ) {
-    version = 'major';
-  }
-  // case: if wording for MINOR found
-  else if (messages.some((message) => minorWords.some((word) => message.includes(word)))) {
-    version = 'minor';
-  }
-  // case: if wording for PATCH found
-  else if (patchWords && messages.some((message) => patchWords.some((word) => message.includes(word)))) {
-    version = 'patch';
-  }
-  // case: if wording for PRE-RELEASE found
-  else if (
-    preReleaseWords &&
-    messages.some((message) =>
-      preReleaseWords.some((word) => {
-        if (message.includes(word)) {
-          foundWord = word;
-          return true;
-        } else {
-          return false;
-        }
-      }),
-    )
-  ) {
-    preid = foundWord.split('-')[1];
-    version = 'prerelease';
-  }
+  
 
-  console.log('version action after first waterfall:', version);
+  // console.log('version action after first waterfall:', version);
 
   // case: if default=prerelease,
   // rc-wording is also set
   // and does not include any of rc-wording
   // then unset it and do not run
-  if (
-    version === 'prerelease' &&
-    preReleaseWords &&
-    !messages.some((message) => preReleaseWords.some((word) => message.includes(word)))
-  ) {
-    version = null;
-  }
+  // if (
+  //   version === 'prerelease' &&
+  //   preReleaseWords &&
+  //   !messages.some((message) => preReleaseWords.some((word) => message.includes(word)))
+  // ) {
+  //   version = null;
+  // }
 
   // case: if default=prerelease, but rc-wording is NOT set
-  if (version === 'prerelease' && preid) {
-    version = 'prerelease';
-    version = `${version} --preid=${preid}`;
-  }
+  // if (version === 'prerelease' && preid) {
+  //   version = 'prerelease';
+  //   version = `${version} --preid=${preid}`;
+  // }
 
-  console.log('version action after final decision:', version);
+  // console.log('version action after final decision:', version);
 
   // case: if nothing of the above matches
-  if (!version) {
-    exitSuccess('No version keywords found, skipping bump.');
-    return;
-  }
+  // if (!version) {
+  //   exitSuccess('No version keywords found, skipping bump.');
+  //   return;
+  // }
 
   // case: if user sets push to false, to skip pushing new tag/package.json
   const push = process.env['INPUT_PUSH'];
